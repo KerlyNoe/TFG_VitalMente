7253f86f9002416a217f32708c453f27f3daa414
KerlyNoe <132671858+KerlyNoe@users.noreply.github.com>
2024-11-18 21:25:11 +0100

    tercera entrega

---
+++ navegadorTres.php
@@ -0,0 +1,50 @@
+<?php
+    //Verificar si hay una sesión activa.
+    if(session_status() === PHP_SESSION_NONE){
+        session_start();
+    }
+
+    //Configuración de conexión con la base de datos.
+    include_once("config.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/estiloshf.css">
+    </head>
+    <body>
+        <header>
+            <div class="logo">
+                <img src="img/logo.png" alt="logo">
+            </div>
+            <nav class="nav">
+                <?php
+                    if(isset($_SESSION['id_usuarios'])) {
+                        $query = "SELECT tipo_usuario FROM usuarios
+                                  WHERE id_usuarios = ?";
+                        $stmt = $conn->prepare($query);
+                        $stmt->bind_param('i', $_SESSION['id_usuarios']);
+                        $stmt->execute();
+                        $tmp = $stmt->get_result();
+                        $usuario = $tmp->fetch_assoc();
+                        $tipo_usuario = $usuario['tipo_usuario'];
+                            //Desde Actividades
+                            if($tipo_usuario === 'normal') {
+                                echo '<a href="inicio.php">Inicio</a>';
+                                echo '<a href="pacientes.php">Perfil</a>';
+                                echo '<a href="cerrar_sesion.php">Cerrar Sesion</a>';
+                            }
+                        $stmt->close();
+                    }else {
+                        echo '<a href="inicio.php">Inicio</a>';
+                        echo '<a href="servicios.php">Servicios</a>';
+                        echo '<a href="actividades.php">Actividades</a>';
+                        echo '<a href="acceder.php">Acceder</a>';
+                    }
+                ?>                                                
+            </nav>
+        </header>
+    </body>
+</html>
